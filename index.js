e4c0e3800def5b7c3bb0c6bb030d742f46b1704d
Ivan <ikladinov@gmail.com>
2022-12-16 17:18:10 -0500

    commit tree db50fcaf8e2e5d2d59cbdc89f159c18a1f0a13e9

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          'name': name,
+          'email': email,
+        }),
+        
+    })
+   .then((response) => response.json())
+  .then((data) => document.body.append(data.id))
+    
+ .catch((error) => document.body.append(error))
+ 
+  //  .catch((error) => {
+  //    console.error('Error:', error);
+  //  });
+}
+
+
+
+/*
+fetch('http://localhost:3000/usewrs')
+.then(response => response.json())
+.then(studentData => {
+    const studentsList = document.querySelector('#students-list')
+
+    studentData.forEach(student => {
+        addStudentsToTheList(student.name)
+ 
+    })
+
+*/
